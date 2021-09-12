18fe48f1150709965eabe8b3450b19c2e1b8474b
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-12 14:54:48 -0700

    useState hook

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View ,Button} from 'react-native';
 
 export default function App() {
+
+  const [name, setname] = useState('Gimna')
+  const [person, setperson] = useState({name:'mario',age:30})
+
+  const clickHandler = () =>{
+      setname('Kavishka')
+      setperson({name:'John',age:'40'})
+
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello world</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>Lorem ipsum dolor <Text>test</Text> sit amet</Text>
-        <Text>Lorem ipsum dolor sit amet</Text>
-        <Text>Lorem ipsum dolor sit amet</Text>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler} />
       </View>
     </View>
   );
@@ -24,15 +31,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header:{
-    backgroundColor:'pink',
-    padding:20
-  },
-  boldText:{
-    fontWeight:'bold'
-  },
-  body:{
-    backgroundColor:'yellow',
-    padding:20
+  buttonContainer:{
+    marginTop:20
   }
 });
