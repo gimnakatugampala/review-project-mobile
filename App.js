675ad13a3c3cf978ac3ed4f06af3bebd69d0dcec
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-14 18:51:49 -0700

    todo app 1

--- App.js
+++ App.js
@@ -1,29 +1,53 @@
 import React,{useState} from 'react';
 import { StyleSheet, Text, View ,Button ,ScrollView, FlatList ,TouchableOpacity} from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
 
-  const [people, setpeople] = useState([
-    {name:'John',id:'1'},
-    {name:'Jack',id:'2'},
-    {name:'Jeff',id:'3'},
-    {name:'Shaun',id:'4'},
-    {name:'Mark',id:'5'},
-    {name:'Bill',id:'6'},
-    {name:'Steve',id:'7'},
-  ])
+  // const [people, setpeople] = useState([
+  //   {name:'John',id:'1'},
+  //   {name:'Jack',id:'2'},
+  //   {name:'Jeff',id:'3'},
+  //   {name:'Shaun',id:'4'},
+  //   {name:'Mark',id:'5'},
+  //   {name:'Bill',id:'6'},
+  //   {name:'Steve',id:'7'},
+  // ])
 
-  const pressHandler = (id) => {
-    console.log(id)
-    setpeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id)
-    })
-  }
+
+  // const pressHandler = (id) => {
+  //   console.log(id)
+  //   setpeople((prevPeople) => {
+  //     return prevPeople.filter(person => person.id != id)
+  //   })
+  // }
+
+  const [todos, settodos] = useState([
+    {text:'buy coffee',key:'1'},
+    {text:'create an app',key:'2'},
+    {text:'play on the switch',key:'3'}
+  ])
 
   return (
     <View style={styles.container}>
 
-      <FlatList 
+      <Header />
+      <View style={styles.content}>
+      {/* to form */}
+      <View style={styles.list}>
+
+        <FlatList
+         data={todos}
+        renderItem={({item}) => (
+          <Text>{item.text}</Text>
+        )}
+        />
+
+      </View>
+      </View>
+
+     
+      {/* <FlatList 
       numColumns={2}
       keyExtractor={(item) => item.id}
         data={people}
@@ -32,7 +56,7 @@ export default function App() {
             <Text style={styles.item} >{item.name}</Text>
           </TouchableOpacity>
         )}
-      />
+      /> */}
 
       {/* <ScrollView>
       {people.map(person => (
@@ -42,7 +66,6 @@ export default function App() {
         )
       )}
       </ScrollView> */}
-     
     </View>
   );
 }
@@ -56,12 +79,18 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center',
   },
-  item:{
-    marginTop:24,
-    padding:30,
-    backgroundColor:'pink',
-    fontSize:24,
-    marginHorizontal:10,
-    marginTop:24
+  content:{
+    padding:40
+  },
+  list:{
+    marginTop:20
   }
+  // item:{
+  //   marginTop:24,
+  //   padding:30,
+  //   backgroundColor:'pink',
+  //   fontSize:24,
+  //   marginHorizontal:10,
+  //   marginTop:24
+  // }
 });
